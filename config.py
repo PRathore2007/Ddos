19ad4610d3fdd6f481236dc9f1ad653364aa2204
PRathore2007 <rathorepiyush2007@gmail.com>
2024-07-16 16:34:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@
 BOT_TOKEN=("7181945956:AAFFIjFv6i2IAnj8GI2nsuEOFGuNLaCWZmE")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("@PiyushR01")
+OWNER_USERNAME=("PiyushR01")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
 ADMIN_IDS=("6239240372")
