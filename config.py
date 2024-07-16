28b4fab385ab9ce5493520f808a5a446fe8e0c09
PRathore2007 <rathorepiyush2007@gmail.com>
2024-07-16 16:34:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7181945956:AAFFIjFv6i2IAnj8GI2nsuEOFGuNLaCWZmE")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("@PiyushR01")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("6239240372")
 
 
 
